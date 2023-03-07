82e5ead7bc815b88b80a198e86cdc34ca643e182
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2023-03-06 23:45:00 -0500

    working, need to make responsive and add revealers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import stateMap from './states.png'
 import './App.css';
+import { cities } from "./cities"
+import { useState } from 'react';
 
-function App() {
-  return (
+const App = () => {
+const [gameState, setGameState] = useState([])
+
+const handleClick = (e) => {
+  let x = e.nativeEvent.offsetX;
+  let y = e.nativeEvent.offsetY;
+  console.log(`xMin: ${e.nativeEvent.offsetX - 12},\n
+              xMax: ${e.nativeEvent.offsetX + 12},\n
+              yMin: ${e.nativeEvent.offsetY - 12},\n
+              yMax: ${e.nativeEvent.offsetY + 12},\n`)
+
+  for (const object of cities) {
+    if (x < object.xMax && x > object.xMin && y > object.yMin && y < object.yMax) {
+      let circle = document.createElement("div");
+      circle.classList.add(`circle${object.place}`);
+      circle.classList.add('circle');
+      document.getElementById("imageBox").appendChild(circle);
+      setGameState([
+        ...gameState,
+        object.place
+      ].sort())
+    }
+  }
+  
+  const revealedCities = cities.filter(obj => {
+    return gameState.some((num) => num === obj.place)
+  })
+
+  if (revealedCities.length === 20) {
+    alert('alright')
+  }
+
+}
+
+const revealer = () => {
+  setGameState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+}
+
+
+
+
+const generateBoard = cities.map((object) =>
+<div key={object.place} className='cityOnBoard' >
+    <h3>{object.place}.</h3>
+    
+    {gameState.indexOf(object.place) > -1 &&
+    <div>
+        <h3>{object.city}</h3>
+        <h4>Population: {object.population}</h4>
+    </div> 
+    }
+</div>
+)
+
+
+
+
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 id='title'>Big City, U.S.A.</h1>
+        <h2 id='directions'>find the 20 largest US cities <br></br>by clicking them on the state map below</h2>
+        <div id='imageBox'>
+          <img src={stateMap} onClick={handleClick} id='stateMap' alt='United States map' />
+        </div>
+        <div id='scoreboard'>
+          {generateBoard}
+        </div>
+        <button onClick={revealer}>reveal all</button>
+        <div id='note'>
+        developer's note 
+          <div id='revealNote'>Cities are ranked by the population of their incorporated area, as oppose to their larger metro areas. This biases the data toward Western cities which have more area. <br></br> Populations based on 2020 census data. For more information <a href='https://en.wikipedia.org/wiki/Cities_and_metropolitan_areas_of_the_United_States'>click here</a>.</div>
+        </div>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ src/cities.js
@@ -0,0 +1,235 @@
+export const cities = [
+    {
+        place: 1,
+        city: "New York",
+        population: "8,804,190",
+        xMax: 625,
+        xMin: 610,
+        yMax: 155,
+        yMin: 140,
+    },
+    {
+        place: 2,
+        city: "Los Angeles",
+        population: "3,898,747",
+        xMax: 40,
+        xMin: 30,
+        yMax: 245,
+        yMin: 230,
+    },
+    {
+        place: 3,
+        city: "Chicago",
+        population: "2,746,388",
+        xMax: 460,
+        xMin: 444,
+        yMax: 165,
+        yMin: 145,
+    },
+    {
+        place: 4,
+        city: "Houston",
+        population: "2,304,580",
+        xMax: 376,
+        xMin: 352,
+        yMax: 370,
+        yMin: 342,
+    },
+   {
+        place: 5,
+        city: "Phoenix",
+        population: "1,607,739",
+        xMin: 125,
+
+        xMax: 149,
+
+        yMin: 263,
+        yMax: 287,
+
+    },
+    {
+        place: 6,
+        city: "Philadelphia",
+        population: "1,603,797",
+        xMin: 591,
+
+              xMax: 615,
+
+              yMin: 151,
+
+        yMax: 175,
+    },    
+    {
+        place: 7,
+        city: "San Antonio",
+        population: "1,434,270",
+        xMin: 300,
+
+              xMax: 324,
+
+              yMin: 343,
+
+              yMax: 367,
+    },
+    {
+        place: 8,
+        city: "San Diego",
+        population: "1,385,922",
+        xMin: 56,
+
+              xMax: 80,
+
+              yMin: 257,
+
+              yMax: 281,
+
+    },    
+    {
+        place: 9,
+        city: "Dallas",
+        population: "1,304,442",
+        xMin: 324,
+
+              xMax: 348,
+
+              yMin: 297,
+
+              yMax: 321,
+    },
+    {
+        place: 10,
+        city: "San Jose",
+        population: "1,014,545",
+        xMin: 14,
+
+              xMax: 38,
+
+              yMin: 178,
+
+              yMax: 202,
+    },    
+    {
+        place: 11,
+        city: "Austin",
+        population: "959,549",
+        xMin: 310,
+
+              xMax: 334,
+
+              yMin: 333,
+
+              yMax: 357,
+    },
+    {
+        place: 12,
+        city: "Jacksonville",
+        population: "949,577",
+        xMin: 540,
+
+              xMax: 564,
+
+              yMin: 321,
+
+              yMax: 345,
+
+    },    
+    {
+        place: 13,
+        city: "Fort Worth",
+        population: "918,377",
+        xMin: 300,
+
+        xMax: 324,
+
+        yMin: 297,
+
+        yMax: 321,
+
+    },
+    {
+        place: 14,
+        city: "Columbus",
+        population: "905,672",
+        xMin: 493,
+
+              xMax: 517,
+
+              yMin: 168,
+
+              yMax: 192,
+    },    
+    {
+        place: 15,
+        city: "Indianapolis",
+        population: "887,752",
+        xMin: 459,
+
+              xMax: 483,
+
+              yMin: 172,
+
+              yMax: 196,
+
+    },
+    {
+        place: 16,
+        city: "Charlotte",
+        population: "874,541",
+        xMin: 537,
+
+              xMax: 561,
+
+              yMin: 238,
+
+              yMax: 262,
+    },    
+    {
+        place: 17,
+        city: "San Francisco",
+        population: "873,965",
+        xMin: 14,
+
+        xMax: 38,
+
+        yMin: 150,
+
+        yMax: 178,
+    },
+    {
+        place: 18,
+        city: "Seattle",
+        population: "735,157",
+        xMin: 54,
+
+              xMax: 78,
+
+              yMin: 16,
+
+              yMax: 40,
+    },
+    {
+        place: 19,
+        city: "Denver",
+        population: "715,522",
+        xMin: 230,
+
+              xMax: 254,
+
+              yMin: 173,
+
+              yMax: 197,
+    },
+    {
+        place: 20,
+        city: "Washington D.C.",
+        population: "689,545",
+        xMin: 574,
+
+              xMax: 598,
+
+              yMin: 173,
+
+              yMax: 197,
+    },
+
+]
